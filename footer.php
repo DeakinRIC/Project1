88f94c2db9877180a308d089a964dfe073a546eb
Harshana <harshanaek@gmail.com>
2015-09-28 23:24:42 +0530

    Latest files

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="clear"></div>
+
+    
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="viewport" content="width=device-width" />
+
+
+    <title><?php wp_title( ' | ', true, 'right' ); ?></title>
+
+    
+
+<?php wp_head(); ?>
+
+</head>
+<body>
